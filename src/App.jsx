2d5d9134ab2760335fae4a6f56eba32c1d6ae08c
Mike_kalange <mikekalange@gmail.com>
2024-11-08 15:13:28 -0500

    new file:   eslint.config.js

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Features from "./Section/Features";
+import Header from "./Section/Header";
+import Hero from "./Section/Hero";
+function App() {
+  return (
+    <>
+      <main className="overflow-hidden">
+        <Header />
+        <Hero />
+        <Features />
+      </main>
+    </>
+  );
+}
+
+export default App;
